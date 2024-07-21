83ae05fff4af9411821037e7ef0fc1f01cc71b6a
Ricky Tang <rickytang2019@gmail.com>
2024-07-21 15:09:21 +0100

    detect and show if it's a yt video page

--- popup.js
+++ popup.js
@@ -1,12 +1,32 @@
+import { getCurrentTab } from "./utils.js";
+
 // adding a new bookmark row to the popup
 const addNewBookmark = () => {};
 
 const viewBookmarks = () => {};
 
-const onPlay = e => {};
+const onPlay = (e) => {};
 
-const onDelete = e => {};
+const onDelete = (e) => {};
 
-const setBookmarkAttributes =  () => {};
+const setBookmarkAttributes = () => {};
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", async () => {
+  const activeTab = await getCurrentTab();
+  const queryParameters = activeTab.url.split("?")[1]; // yt video id
+  const urlParameters = new URLSearchParams(queryParameters);
+
+  const currentVideo = urlParameters.get("v");
+
+  if (activeTab.url.includes("youtube.com/watch") && currentVideo) {
+    chrome.storage.sync.get([currentVideo], (data) => {
+      const currentVideoBookmarks = data[currentVideo]
+        ? JSON.parse(data[currentVideo])
+        : [];
+    });
+  } else {
+    document.querySelector(
+      ".container"
+    ).innerHTML = `<div class="title">This is not a youtube video page.</div>`;
+  }
+});
