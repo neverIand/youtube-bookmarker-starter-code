83a50016f1239787d4d886ceb8f2c29d728d54c3
Ricky Tang <rickytang2019@gmail.com>
2024-07-21 14:49:58 +0100

    get bookmarks of current vid from chrome storage

--- contentScript.js
+++ contentScript.js
@@ -13,10 +13,19 @@
     }
   });
 
-  const newVideoLoaded = () => {
+  const fetchBookmarks = () => {
+    return new Promise((resolve) => {
+      chrome.storage.sync.get([currentVideo], (obj) => {
+        resolve(obj[currentVideo] ? JSON.parse(obj[currentVideo]) : []);
+      });
+    });
+  };
+
+  const newVideoLoaded = async () => {
     const bookmarkBtnExists =
       document.getElementsByClassName("bookmark-btn")[0];
-    console.log(bookmarkBtnExists);
+    // console.log(bookmarkBtnExists);
+    currentVideoBookmarks = await fetchBookmarks();
 
     if (bookmarkBtnExists) {
       return;
@@ -38,13 +47,15 @@
     bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
   };
 
-  const addNewBookmarkEventHandler = () => {
+  const addNewBookmarkEventHandler = async () => {
     const currentTime = youtubePlayer.currentTime;
     const newBookmark = {
       time: currentTime,
       desc: "Bookmark at " + getTime(currentTime),
     };
     console.log("newBookmark", newBookmark);
+
+    currentVideoBookmarks = await fetchBookmarks();
 
     chrome.storage.sync.set({
       [currentVideo]: JSON.stringify(
---
+++ utils.js
@@ -0,0 +1,6 @@
+// see api docs
+export async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
